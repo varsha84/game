d200af7be67dfb72313caa1c74dc463ecce94d4b
varsha85 <varsha.verma85@gmail.com>
2022-08-09 22:35:34 +0200

    adding second version of falling letters game

--- src/App.js
+++ src/App.js
@@ -1,103 +1,80 @@
-import React, {useRef,  useState, useEffect } from "react";
-import "./App.css";
-import BlockComponent from "./BlockComonent";
 
-const speedOfFallingBlock = 2000;
+import React, { useState, useEffect } from "react";
+import BlockComponent from './BlockComonent';
+
+import "./App.css";
 
 function App() {
   const speedOfNewBlock = 2000;
-  const [letterA, setLetterA] = useState([]);
-  const [letterB, setLetterB] = useState([]);
-  const [letterC, setLetterC] = useState([]);
-  const [letterD, setLetterD] = useState([]);
-  const [letterE, setLetterE] = useState([]);
+  const [playAreaList, setPlayAreaList] = useState([]);
   const [pavilionList, setPavilionList] = useState([]);
-  
-  
-  let myInterval = null;
-  const alphabet = "a";
-  const letter = alphabet[Math.floor(Math.random() * alphabet.length)].toUpperCase();
+  const alphabet = "abcde";
+  const letter = alphabet[Math.floor(Math.random() * alphabet.length)].toUpperCase(); 
 
+  
   function handleKeyDown(e) {
-
     console.log(e.keyCode);
-    if ((e.keyCode === 65) && (letterA.length > 1)){
-        setLetterA([]);
-    } else if ((e.keyCode === 66) && (letterB.length > 1)){
-      setLetterB([]);
-    } else if ((e.keyCode === 67) && (letterC.length > 1)){
-      setLetterC([]);
-    } else if ((e.keyCode === 68) && (letterD.length > 1)){
-      setLetterD([]);
-    } else if ((e.keyCode === 69) && (letterE.length > 1)){
-      setLetterE([]);
+    if(e.keyCode === 65){
+      let newlist = playAreaList.filter( el => el.props.letter === "A")
+      if (newlist.length > 1){
+        setPlayAreaList(playAreaList.filter( el => el.props.letter !== "A"))
+      }
+    }
+    if(e.keyCode === 66){
+      let newlist = playAreaList.filter( el => el.props.letter === "B")
+      if (newlist.length > 1){
+        setPlayAreaList(playAreaList.filter( el => el.props.letter !== "B"))
+      }
+    }
+    if(e.keyCode === 67){
+      let newlist = playAreaList.filter( el => el.props.letter === "C")
+      if (newlist.length > 1){
+        setPlayAreaList(playAreaList.filter( el => el.props.letter !== "C"))
+      }
+    }
+    if(e.keyCode === 68){
+      let newlist = playAreaList.filter( el => el.props.letter === "D")
+      if (newlist.length > 1){
+        setPlayAreaList(playAreaList.filter( el => el.props.letter !== "D"))
+      }
+    }
+    if(e.keyCode === 69){
+      let newlist = playAreaList.filter( el => el.props.letter === "E")
+      if (newlist.length > 1){
+        setPlayAreaList(playAreaList.filter( el => el.props.letter !== "E"))
+      }
     }
   }
-  
-  
 
   useEffect(() => {
     const timer = window.setTimeout(() => {
-      const startTopPos =Math.floor(Math.random() * (450));
-      const el = <BlockComponent letter={letter} startTopPos={startTopPos}/> ;
-      //console.log(el);
-      //console.log(el.props.getTop(startTopPos));
-
-      if (letter === "A") {
-        setLetterA([...letterA, el]);
-      } else if (letter === "B") {
-        setLetterB([...letterB, el]);
-      } else if (letter === "C") {
-        setLetterC([...letterC, el]);
-      } else if (letter === "D") {
-        setLetterD([...letterD, el]);
-      } else if (letter === "E") {
-        setLetterE([...letterE, el]);
-      }
-      
+      const el = <BlockComponent letter={letter}/>
+      console.log(el)
+      setPlayAreaList([...playAreaList, el])
     }, speedOfNewBlock);
 
     document.addEventListener("keydown", handleKeyDown);
 
-    const timer2 = window.setTimeout(()=>{
-      let value = 0
-      console.log(letterA);
-      console.log("checking top");
-      console.log(letterA[0]);
-      
-    },speedOfFallingBlock)
-      
-
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
       window.clearTimeout(timer);
-      window.clearTimeout(timer2);
     };
-    
+  }, [playAreaList]);
 
-  }, [letterA, letterB, letterC, letterD, letterE]);
-
-  
-
+ 
   return (
     <div className="game-containter">
       <div>
         <div className="play-ground" id="playarea">
-          {letterA}
-          {letterB}
-          {letterC}
-          {letterD}
-          {letterE}
+         {playAreaList}
         </div>
-        <div className="pavilion">{pavilionList}</div>
-        {/* <button onClick={handleStartGame}>Start</button>
-        <button onClick={handleStopGame}>Stop</button> */}
+        <div className="pavilion">
+          {pavilionList}
+        </div>
       </div>
       <div className="score"></div>
     </div>
   );
 }
-
-
 
 export default App;
--- src/BlockComonent.js
+++ src/BlockComonent.js
@@ -1,53 +1,60 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-export default function BlockComponent({ letter, startTopPos}) {
-  //const contextValue = React.useContext(context);
+const BlockComponent = ({ letter}) => {
   const speedOfFallingBlock = 2000;
-   const playarea_width = 500;
+  const playarea_width = 500;
   const playarea_height = 500;
   const colors = ["#ff0000", "#00ff00", "#0000ff"];
   const sizes = [30, 40, 50];
+
   const [bgColor, setBgColor] = useState(colors[Math.floor(Math.random() * colors.length)]);
   const [boxSize, setBoxSize] = useState(sizes[Math.floor(Math.random() * sizes.length)]);
-  const [left, setLeft] = useState( Math.floor(Math.random() * (playarea_width - boxSize)));
-  //const [top, setTop] = useState(Math.floor(Math.random() * (playarea_height - boxSize)));
-  const [top, setTop] = useState(startTopPos);
+  const [left, setLeft] = useState(Math.floor(Math.random() * (playarea_width - boxSize)))
+  const [top, setTop] = useState(Math.floor(Math.random() * (playarea_height - boxSize)))
+  //const [top, setTop] = useState(topPos)
   const fontSize = boxSize - 10; // font size must be 10 px less than box size
+  const [moveOut, setMoveOut] = useState(false)
 
-  useEffect(() => {
-    const timer = window.setTimeout(() => {
-      setTop(top + 10);
-    }, speedOfFallingBlock);
-    //console.log("top = " + top);
-    return () => {
-      window.clearTimeout(timer);
-    };
-  }, [top]); 
+  useEffect(()=>{
+    const timer = window.setTimeout(() => { 
+      if (top + boxSize < playarea_height){
+        const newTop = top + 10;
+        setTop(newTop);
+      }
+      else{
+        setTop(playarea_height)
+        setMoveOut(true)
+      }      
+    }, 2000)
+    
+    return () => {  window.clearTimeout(timer);};
+  },[top])
   
-
   return (
     <div
       style={{
-        width: `${boxSize}` + "px",
-        height: `${boxSize}` + "px",
+        width: `${boxSize}` +"px",
+        height: `${boxSize}` +"px",
         "background-color": `${bgColor}`,
-        left: `${left}` + "px",
-        top: `${top}` + "px",
+        left: `${left}` +"px",
+        top: `${top}`+"px",
         position: "absolute",
         "text-align": "center",
-        "font-size": `${fontSize}` + "px",
+        "font-size": `${fontSize}`+"px",
       }}
     >
       {letter}
     </div>
   );
 };
-
 BlockComponent.propTypes = {
-  getTop: PropTypes.func,
+  letter: PropTypes.string.isRequired,
+  getMoveOut: PropTypes.func,
 };
 
 BlockComponent.defaultProps = {
-  getTop: (top) => top,
-}
+  letter: "A",  
+  getMoveOut: (value) => value,
+};
+export default BlockComponent;
